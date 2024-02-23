1591a011d393357ab41a9338951d1d385f696328
zhangying169 <zhangying169@meituan.com>
2024-02-23 18:15:00 +0800

    support lm_evaluation_harness

---
+++ autosmoothquant/examples/eval_model.py
@@ -0,0 +1,107 @@
+
+import torch
+import os
+from tqdm import tqdm
+import argparse
+import collections
+
+from benchmarks import evaluator
+from benchmarks.models.quant_model import quant_model
+from benchmarks.utils import make_table, pattern_match
+from lm_eval.tasks import TaskManager
+from autosmoothquant.utils import parse_quant_config, get_loaders
+
+def parse_args():                                                                                                                                                                                                                                                                                                                               
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model-path', type=str,
+                        default='int8-models/llama-13b', help='path contains model weight and quant config')
+    parser.add_argument('--tokenizer-path', type=str,
+                        default='int8-models/llama-13b', help='path contains tokenizer')
+    parser.add_argument("--tasks", default="")
+    parser.add_argument("--eval_ppl", action="store_true", default=True)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--limit", type=int, default=None)
+    parser.add_argument("--num_fewshot", type=int, default=0)
+    parser.add_argument("--seed", type=int, default=0)
+    return parser.parse_args()
+
+
+def update_results(results, new_result):
+    for key, value in new_result.items():
+        if key in results:
+            results[key].update(value)
+        else:
+            results.update({key: value})
+
+
+@torch.no_grad()
+def benchmarks(lm, args):
+    # for task in ["wikitext2", "c4"]:
+    results = {}
+    if args.eval_ppl:
+        for task in ["wikitext2"]:
+            _, testloader = get_loaders(
+                task,
+                seed=args.seed,
+                model=args.tokenizer_path,
+                seqlen=lm.max_length,
+            )
+            if "c4" in task:
+                testenc = testloader
+            else:
+                testenc = testloader.input_ids
+
+            nsamples = testenc.numel() // lm.max_length
+            lm.model.eval()
+            nlls = []
+            for i in tqdm(range(nsamples)):
+                batched_inps = testenc[
+                    :, (i * lm.max_length) : ((i + 1) * lm.max_length)
+                ].to("cuda:0")
+                batched_labels = testenc[
+                    :, (i * lm.max_length) : ((i + 1) * lm.max_length)
+                ].to(lm.model.lm_head.weight.device)
+                loss = lm.model(batched_inps, labels=batched_labels).loss
+                neg_log_likelihood = loss.float() * lm.max_length
+                nlls.append(neg_log_likelihood)
+                if i == args.limit:
+                    break
+
+            ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * lm.max_length))
+
+            result = collections.defaultdict(dict)
+            versions = collections.defaultdict(dict)
+            n_shot = collections.defaultdict(dict)
+            result[task]["ppl"] = ppl.item()
+            versions[task] = 0
+            n_shot[task] = 0
+            t_results = {
+                "results": dict(result),
+                "versions": dict(versions),
+                "n-shot": dict(n_shot),
+            }
+            update_results(results, t_results)
+    if args.tasks != "":
+        t_results = evaluator.simple_evaluate(
+            lm,
+            tasks=pattern_match(args.tasks.split(","), TaskManager().all_tasks),
+            num_fewshot=args.num_fewshot,
+            limit=args.limit,
+        )
+        update_results(results, t_results)
+    return results
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config_path = os.path.join(args.model_path, "quant_config.json")
+    quant_config = parse_quant_config(config_path)
+
+    lm = quant_model(
+        pretrained=args.model_path,
+        batch_size=args.batch_size,
+        quant_config=quant_config,
+        tokenizer=args.tokenizer_path,
+    )
+    results = benchmarks(lm, args)
+    print(make_table(results))
---
+++ benchmarks/base.py
@@ -0,0 +1,272 @@
+import abc
+from typing import Iterable
+import numpy as np
+import random
+import re
+import os
+import json
+import hashlib
+import datasets
+from sqlitedict import SqliteDict
+from tqdm import tqdm
+import torch
+import torch.nn.functional as F
+
+from lm_eval.metrics import mean, weighted_perplexity, weighted_mean, bits_per_byte
+from . import utils
+from abc import abstractmethod
+from lm_eval.api.model import LM
+
+
+class BaseLM(LM):
+    @property
+    @abstractmethod
+    def eot_token_id(self):
+        pass
+
+    @property
+    @abstractmethod
+    def max_length(self):
+        pass
+
+    @property
+    @abstractmethod
+    def max_gen_toks(self):
+        pass
+
+    @property
+    @abstractmethod
+    def batch_size(self):
+        pass
+
+    @property
+    @abstractmethod
+    def device(self):
+        pass
+
+    @abstractmethod
+    def tok_encode(self, string: str):
+        pass
+
+    @abstractmethod
+    def tok_decode(self, tokens: Iterable[int]):
+        pass
+
+    @abstractmethod
+    def _model_generate(self, context, max_length, eos_token_id):
+        pass
+
+    @abstractmethod
+    def _model_call(self, inps):
+        """
+        inps: a torch tensor of shape [batch, sequence]
+        the size of sequence may vary from call to call
+
+        returns: a torch tensor of shape [batch, sequence, vocab] with the
+        logits returned from the model
+        """
+        pass
+
+    # subclass must implement properties vocab_size, eot_token_id, max_gen_toks, batch_size, device, max_length.
+    # TODO: enforce this somehow
+
+    def loglikelihood(self, requests):
+        new_reqs = []
+        for context, continuation in requests:
+            if context == "":
+                # end of text as context
+                context_enc = [self.eot_token_id]
+            else:
+                context_enc = self.tok_encode(context)
+
+            continuation_enc = self.tok_encode(continuation)
+
+            new_reqs.append(((context, continuation), context_enc, continuation_enc))
+
+        return self._loglikelihood_tokens(new_reqs)
+
+    def loglikelihood_rolling(self, requests):
+        # TODO: Implement caching once we've confirmed the perplexity implementation
+        # TODO: automatic batch size detection for vectorization
+
+        loglikelihoods = []
+        for (string,) in tqdm(requests):
+            rolling_token_windows = list(
+                map(
+                    utils.make_disjoint_window,
+                    utils.get_rolling_token_windows(
+                        token_list=self.tok_encode(string),
+                        prefix_token=self.eot_token_id,
+                        max_seq_len=self.max_length,
+                        context_len=1,
+                    ),
+                )
+            )
+
+            rolling_token_windows = [(None,) + x for x in rolling_token_windows]
+
+            # TODO: extract out this call so it only gets called once and also somehow figure out partial caching for
+            # that
+            string_nll = self._loglikelihood_tokens(
+                rolling_token_windows, disable_tqdm=True
+            )
+
+            # discard is_greedy
+            string_nll = [x[0] for x in string_nll]
+
+            string_nll = sum(string_nll)
+            loglikelihoods.append(string_nll)
+
+        return loglikelihoods
+
+    def _loglikelihood_tokens(self, requests, disable_tqdm=False):
+        # TODO: implement some kind of efficient-request-middleware that lumps together requests with the same context
+        res = []
+
+        def _collate(x):
+            # the negative sign on len(toks) sorts descending - this has a few advantages:
+            # - time estimates will always be over not underestimates, which is more useful for planning
+            # - to know the size of a batch when going through the list, you know the first one is always the batch
+            #   padded context length. this is useful to simplify the batching logic and more importantly to make
+            #   automatic adaptive batches much much easier to implement
+            # - any OOMs will happen right away rather than near the end
+
+            toks = x[1] + x[2]
+            return -len(toks), tuple(toks)
+
+        # TODO: automatic (variable) batch size detection for vectorization
+        re_ord = utils.Reorderer(requests, _collate)
+        for chunk in utils.chunks(
+            tqdm(re_ord.get_reordered(), disable=disable_tqdm), self.batch_size
+        ):
+            inps = []
+            cont_toks_list = []
+            inplens = []
+
+            padding_length = None
+
+            # because vectorizing is annoying, we first convert each (context, continuation) pair to padded
+            # tensors, then we pack them together into a batch, call the model, and then pick it all apart
+            # again because vectorizing is annoying
+
+            for _, context_enc, continuation_enc in chunk:
+                # sanity check
+                assert len(context_enc) > 0
+                assert len(continuation_enc) > 0
+                assert len(continuation_enc) <= self.max_length
+
+                # how this all works:
+                #          CTX      CONT
+                # inp    0 1 2 3|4 5 6 7 8 9   <- last token is deleted by inp[:, :-1]
+                # gpt2    \               \
+                # logits   1 2 3|4 5 6 7 8 9   <- the ctx half gets tossed out by the
+                # cont_toks      4 5 6 7 8 9      [:, -len(continuation_enc):, :self.vocab_size] slice
+
+                # when too long to fit in context, truncate from the left
+                inp = torch.tensor(
+                    (context_enc + continuation_enc)[-(self.max_length + 1) :][:-1],
+                    dtype=torch.long,
+                ).to(self.device)
+                (inplen,) = inp.shape
+
+                cont = continuation_enc
+
+                # since in _collate we make sure length is descending, the longest is always the first one.
+                padding_length = (
+                    padding_length if padding_length is not None else inplen
+                )
+
+                # pad length from seq to padding_length
+                inp = torch.cat(
+                    [
+                        inp,  # [seq]
+                        torch.zeros(padding_length - inplen, dtype=torch.long).to(
+                            inp.device
+                        ),  # [padding_length - seq]
+                    ],
+                    dim=0,
+                )
+
+                inps.append(inp.unsqueeze(0))  # [1, padding_length]
+                cont_toks_list.append(cont)
+                inplens.append(inplen)
+
+            batched_inps = torch.cat(inps, dim=0)  # [batch, padding_length
+            attention_mask = batched_inps.new_zeros(batched_inps.shape)
+            attention_mask[batched_inps != 0] = 1
+            multi_logits = F.log_softmax(
+                self._model_call(batched_inps, attention_mask), dim=-1
+            ).cpu()  # [batch, padding_length, vocab]
+
+            for (cache_key, _, _), logits, inp, inplen, cont_toks in zip(
+                chunk, multi_logits, inps, inplens, cont_toks_list
+            ):
+
+                # Slice to original seq length
+                contlen = len(cont_toks)
+                logits = logits[inplen - contlen : inplen].unsqueeze(
+                    0
+                )  # [1, seq, vocab]
+
+                # Check if per-token argmax is exactly equal to continuation
+                greedy_tokens = logits.argmax(dim=-1)
+                cont_toks = torch.tensor(cont_toks, dtype=torch.long).unsqueeze(
+                    0
+                )  # [1, seq]
+                max_equal = (greedy_tokens == cont_toks).all()
+
+                # Obtain log-probs at the corresponding continuation token indices
+                # last_token_slice = logits[:, -1, :].squeeze(0).tolist()
+                logits = torch.gather(logits, 2, cont_toks.unsqueeze(-1)).squeeze(
+                    -1
+                )  # [1, seq]
+
+                # Answer: (log prob, is-exact-match)
+                answer = (float(logits.sum()), bool(max_equal))
+
+                # partial caching
+                if cache_key is not None:
+                    self.cache_hook.add_partial("loglikelihood", cache_key, answer)
+
+                res.append(answer)
+
+        return re_ord.get_original(res)
+
+    def generate_until(self, requests):
+        # TODO: implement fully general `until` that handles until that are
+        #       multiple tokens or that span multiple tokens correctly
+
+        # TODO: extract to TokenizedLM?
+        res = []
+
+        def _collate(x):
+            toks = self.tok_encode(x[0])
+            return len(toks), x[0]
+
+        re_ord = utils.Reorderer(requests, _collate)
+
+        for context, until in tqdm(re_ord.get_reordered()):
+            if isinstance(until, str):
+                until = [until]
+
+            (primary_until,) = self.tok_encode(until[0])
+
+            context_enc = torch.tensor(
+                [self.tok_encode(context)[self.max_gen_toks - self.max_length :]]
+            ).to(self.device)
+
+            cont = self._model_generate(
+                context_enc, context_enc.shape[1] + self.max_gen_toks, primary_until
+            )
+
+            s = self.tok_decode(cont[0].tolist()[context_enc.shape[1] :])
+
+            for term in until:
+                s = s.split(term)[0]
+
+            # partial caching
+            self.cache_hook.add_partial("generate_until", (context, until), s)
+
+            res.append(s)
+
+        return re_ord.get_original(res)
